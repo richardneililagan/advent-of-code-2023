cadd0aea9756c9f1c8d527ba29e2ca1a39f154a0
Richard Neil Ilagan <705310+richardneililagan@users.noreply.github.com>
2023-12-04 10:33:10 +1100

    feat: working input parsing

---
+++ solutions/aoc_01/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let lines = input_parser::parse_input("inputs/01a.txt");
+    for line in lines {
+        println!("{}", line);
+    }
+}
---
+++ utils/input_parser/src/lib.rs
@@ -0,0 +1,30 @@
+use std::fs;
+use std::path;
+
+pub fn parse_input(filepath: &str) -> Vec<String> {
+    fs::read_to_string(get_input_filepath(filepath))
+        .expect("Something went wrong reading the file")
+        .split_whitespace()
+        .map(str::to_string)
+        .collect::<Vec<String>>()
+}
+
+fn get_input_filepath(filepath: &str) -> path::PathBuf {
+    let current_dir = std::env::current_dir().expect("Failed to get current path");
+    println!("{}", current_dir.display());
+    current_dir
+        .join(filepath)
+        .canonicalize()
+        .expect("Failed to get canonical path")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = parse_input("test/data.txt");
+        assert_eq!(result, vec!["a", "bc", "def", "gh", "ij"]);
+    }
+}
